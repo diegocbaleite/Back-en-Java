5a26c3203062c84689041620ab249bb672b90fe3
Diego Assunção <dev.diegocba@gmail.com>
2026-02-09 20:25:46 -0400

    Primeiro Commit

---
+++ src/main/java/com/leite/java/back/end/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.leite.java.back.end.controller;
+
+import com.leite.java.back.end.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+import jakarta.annotation.PostConstruct;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private static List<UserDTO> usuarios = new ArrayList<>();
+
+    @GetMapping("/")
+    public String getMensagem() {
+        return "Spring Boot está funcionando!";
+    }
+
+    @GetMapping("/listar")
+    public List<UserDTO> listarUsuarios() {
+        return usuarios;
+    }
+
+    @GetMapping("/{cpf}")
+    public UserDTO filtroPorCpf(@PathVariable String cpf) {
+        return usuarios
+                .stream()
+                .filter(userDTO -> userDTO.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+    }
+
+    @PostConstruct
+    public void iniciarLista() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome("Diego Assunção");
+        userDTO.setCpf("123");
+        userDTO.setEndereco("Rua a");
+        userDTO.setEmail("eduardo@email.com");
+        userDTO.setTelefone("1234-3454");
+        userDTO.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setNome("Luiz Henrique");
+        userDTO2.setCpf("456");
+        userDTO2.setEndereco("Rua b");
+        userDTO2.setEmail("luiz@email.com");
+        userDTO2.setTelefone("1234-3454");
+        userDTO2.setDataCadastro(LocalDateTime.now());
+
+        UserDTO userDTO3 = new UserDTO();
+        userDTO3.setNome("Bruna Santos");
+        userDTO3.setCpf("678");
+        userDTO3.setEndereco("Rua c");
+        userDTO3.setEmail("bruna@email.com");
+        userDTO3.setTelefone("1234-3454");
+        userDTO3.setDataCadastro(LocalDateTime.now());
+
+        usuarios.add(userDTO);
+        usuarios.add(userDTO2);
+        usuarios.add(userDTO3);
+
+    }
+}
+
---
+++ src/test/java/com/leite/java/back/end/model/User.java
@@ -0,0 +1,32 @@
+package com.leite.java.back.end.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String key;
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+
+}
